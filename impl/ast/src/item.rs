6fe24ef4bac29e0ef8af0c9fe7f634e0596973ea
vyacheslavhere <slavlendstudio@gmail.com>
2026-02-21 10:21:11 +0500

    update ast

--- impl/ast/src/item.rs
+++ impl/ast/src/item.rs
@@ -3,7 +3,7 @@ use crate::atom::{Param, TypeHint};
 use common::token::Span;
 
 /// Represents struct field
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Field {
     pub span: Span,
     pub name: String,
@@ -11,16 +11,15 @@ pub struct Field {
 }
 
 /// Represents struct top-level item
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Struct {
-    pub span: Span,
     pub name: String,
     pub generics: Vec<String>,
     pub fields: Vec<Field>,
 }
 
 /// Represents enum variant
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Variant {
     pub span: Span,
     pub name: String,
@@ -28,27 +27,32 @@ pub struct Variant {
 }
 
 /// Represents enum top-level item
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Enum {
-    pub span: Span,
     pub name: String,
     pub generics: Vec<String>,
     pub variants: Vec<Variant>,
 }
 
 /// Function top-level item
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Function {
-    pub span: Span,
     pub name: String,
     pub generics: Vec<String>,
     pub params: Vec<Param>,
 }
 
-/// Top-level item
-#[derive(Debug, Clone)]
-pub enum Item {
+/// Top-level item kind
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ItemKind {
     Struct(Struct),
     Enum(Enum),
     Function(Function),
+}
+
+/// Top-level item
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Item {
+    pub kind: ItemKind,
+    pub span: Span,
 }
