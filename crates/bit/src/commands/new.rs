ca2f344eb34feaeb04dbc88b997113565dbf154d
vyacheslavhere <slavlendstudio@gmail.com>
2026-02-25 11:30:28 +0500

    fix `new`

--- crates/bit/src/commands/new.rs
+++ crates/bit/src/commands/new.rs
@@ -1,11 +1,19 @@
 /// Imports
+use crate::errors::CliError;
+use bit_common::bail;
 use bit_pm::{config::PackageType, generate};
+use std::io::ErrorKind;
 
 /// Executes command
 pub fn execute(path: String, pkg_ty: Option<PackageType>) {
-    std::fs::create_dir(&path).unwrap();
-
-    let pkg_ty = pkg_ty.unwrap_or(PackageType::App);
-
-    generate::gen_project(path.into(), pkg_ty);
+    match std::fs::create_dir(&path) {
+        Ok(_) => {
+            let pkg_ty = pkg_ty.unwrap_or(PackageType::App);
+            generate::gen_project(path.into(), pkg_ty);
+        }
+        Err(error) => match error.kind() {
+            ErrorKind::AlreadyExists => bail!(CliError::AlreadyExists { path }),
+            kind => bail!(CliError::FailedToCreateDir { path, kind }),
+        },
+    };
 }
--- crates/bit/src/errors.rs
+++ crates/bit/src/errors.rs
@@ -1,17 +1,23 @@
 /// Imports
 use miette::Diagnostic;
-use std::path::PathBuf;
+use std::{io::ErrorKind, path::PathBuf};
 use thiserror::Error;
 
 /// Cli error
 #[derive(Debug, Error, Diagnostic)]
 pub enum CliError {
-    #[error("failed to retrieve current working directory.")]
+    #[error("failed to retrieve cwd.")]
     #[diagnostic(
         code(pkg::failed_to_retrieve_cwd),
         help("check existence of current working directory.")
     )]
     FailedToRetrieveCwd,
+    #[error("{path} already exists.")]
+    #[diagnostic(code(pkg::already_exists))]
+    AlreadyExists { path: String },
+    #[error("failed to create directory {path}: {kind}.")]
+    #[diagnostic(code(pkg::failed_to_create_dir))]
+    FailedToCreateDir { path: String, kind: ErrorKind },
     #[error("failed to convert path {path} to utf8 path.")]
     #[diagnostic(code(pkg::wrong_utf8_path))]
     WrongUtf8Path { path: PathBuf },
